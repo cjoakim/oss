286660fb490c63be80db5b38d1610ceb81bfd502
Chris Joakim <christopher.joakim@gmail.com>
2023-09-16 14:46:18 -0400

    Distante.test.ts

--- m26-js-ts/src/Distance.test.ts
+++ m26-js-ts/src/Distance.test.ts
@@ -23,53 +23,37 @@ test("constructor miles uom", () => {
 });
 
 test("constructor kilometers uom", () => {
-    // d = new Distance(10.0, 'k');
-    // expect(d.uom()).toBe(Constants.UOM_KILOMETERS);
-    // expect(d.as_miles()).isWithin(0.0000000001, 6.2137119223733395);
-    // expect(d.as_kilometers()).isWithin(0.0000000001, 10.0);
-    // return expect(d.as_yards()).isWithin(0.000001, 10936.132983377078);
+    let d = new Distance(10.0, Constants.UOM_KILOMETERS);
+    expect(d.value).toBe(10.0);
+    expect(d.uom).toBe(Constants.UOM_KILOMETERS);
 
-    let d = new Distance(26.2, Constants.UOM_MILES);
-    expect(d.value).toBe(26.2);
-    expect(d.uom).toBe(Constants.UOM_MILES);
-
-    expect(d.asMiles()).toBe(26.2);
-    expect(d.asKilometers()).toBe(42.1648128);
-    expect(d.asYards()).toBe(46112.0);
+    expect(d.asMiles()).toBe(6.2137119223733395);
+    expect(d.asKilometers()).toBe(10.0);
+    expect(d.asYards()).toBe(10936.132983377078);
 });
 
 test("constructor yards uom", () => {
-    // d1 = new Distance(26.2);
-    // d2 = new Distance(4.8);
-    // d3 = d1.add(d2);
-    // expect(d3.uom()).toBe(Constants.UOM_MILES);
-    // return expect(d3.as_miles()).isWithin(0.0000000001, 31.0);
+    let d = new Distance(1800, Constants.UOM_YARDS);
+    expect(d.value).toBe(1800);
+    expect(d.uom).toBe(Constants.UOM_YARDS);
 
-    let d = new Distance(26.2, Constants.UOM_MILES);
-    expect(d.value).toBe(26.2);
-    expect(d.uom).toBe(Constants.UOM_MILES);
-
-    expect(d.asMiles()).toBe(26.2);
-    expect(d.asKilometers()).toBe(42.1648128);
-    expect(d.asYards()).toBe(46112.0);
+    expect(d.asMiles()).toBe(1.0227272727272727);
+    expect(d.asKilometers()).toBe(1.64592);
+    expect(d.asYards()).toBe(1800);
 });
 
 test("add", () => {
-    // d1 = new Distance(26.2);
-    // d2 = new Distance(4.8);
-    // d3 = d1.add(d2);
-    // expect(d3.uom()).toBe(Constants.UOM_MILES);
-    // return expect(d3.as_miles()).isWithin(0.0000000001, 31.0);
-
-    let d = new Distance(26.2, Constants.UOM_MILES);
+    let d1 = new Distance(26.2);
+    let d2 = new Distance(4.8);
+    let d3 = d1.add(d2);
+    expect(d3.uom).toBe(Constants.UOM_MILES);
+    expect(d3.asMiles()).toBe(31.0);
 });
 
 test("subtract", () => {
-    // d1 = new Distance(26.2);
-    // d2 = new Distance(10.0, 'k');
-    // d3 = d1.subtract(d2);
-    // expect(d3.uom()).toBe(Constants.UOM_MILES);
-    // return expect(d3.as_miles()).isWithin(0.0000000001, 19.98628807762666);
-
-    let d = new Distance(26.2, Constants.UOM_MILES);
+    let d1 = new Distance(26.2);
+    let d2 = new Distance(10.0, 'k');
+    let d3 = d1.subtract(d2);
+    expect(d3.uom).toBe(Constants.UOM_MILES);
+    expect(d3.asMiles()).toBe(19.98628807762666);
 });
