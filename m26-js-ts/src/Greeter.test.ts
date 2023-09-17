0ad2f89d4b6e3424d48beaf3878f37174535f121
Chris Joakim <christopher.joakim@gmail.com>
2023-09-17 07:17:43 -0400

    removed Greeter.ts

--- m26-js-ts/src/Greeter.test.ts
+++
@@ -1,8 +0,0 @@
-import { Greeter } from "./Greeter";
-
-test("greet", () => {
-    let g = new Greeter();
-    let s = g.greet('Miles') 
-    let expected = 'hello Miles!';
-    expect(s).toBe(expected);
-});
